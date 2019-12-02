d294cc99dd2447e82ba0dbba7b99bf128bca1f06
Aman <aman.patni@cerner.com>
2019-12-02 23:43:19 +0530

    My first

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+#create a blog model 
+#title
+#pub_date
+#body_blog_post
+#image_blog_post
+
+class blog(models.Model)
+    title=
+
+# add the blog to the settings
+# create a migration
+#migrate
+# add to the admin
